d1a9601352475e77a1b3c489b9677f8ce6f9cb7f
Mansi Rajput <125797750+Mansi-Rajput@users.noreply.github.com>
2024-07-01 11:55:43 +0530

    Task completed

---
+++ decorator.py
@@ -0,0 +1,21 @@
+import time
+
+def time_decorator(func):
+    def wrapper(*args, **kwargs):
+        """This function will execute inside the main function"""
+        start_time = time.time() #calculating the start time
+        func(*args, **kwargs) #calling the function to execute
+        end_time = time.time() #calculating the end time
+        exec_time = end_time - start_time #calculating execution time
+        print(f"Time taken by {func.__name__} to execute: {exec_time:.4f} seconds")
+    return wrapper
+
+#decorating the function
+@time_decorator
+def slow_func():
+    """This is the decorated function"""
+    time.sleep(2)
+    print("Function completed executing.")
+
+#calling the decorated function
+slow_func()

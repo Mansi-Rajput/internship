8e1a91ef93200e579cf8e235a0d20510127f8478
ShivamAMIN008 <167624153+ShivamAMIN008@users.noreply.github.com>
2024-07-08 11:32:26 +0530

    shivam

--- shivam/MetaClass.py
+++ shivam/MetaClass.py
@@ -1,9 +1,10 @@
 class SingletonMeta(object):
-  def __init__(self,ftype):
-      self.ftype =ftype #Storing the provided type("Doubleton" in this case)
+      def __init__(self,ftype):  
+          self.ftype =ftype #Storing the provided type("Doubleton" in this case)
+          
 
-  def getFtype(self):
-      return self.ftype #Return the stored type
+      def getFtype(self):
+          return self.ftype #Return the stored type
 
 def main():
     fType = SingletonMeta(ftype = "Doubleton") #Create a SingletonMeta instance
